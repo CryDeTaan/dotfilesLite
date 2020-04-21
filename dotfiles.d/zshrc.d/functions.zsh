59c3eabc8359c47b86746d0022ee06a85e85082d
CryDeTaan <crydetaan@gmail.com>
2020-04-21 09:14:37 +0200

    Some stuff 2

---
+++ dotfiles.d/zshrc.d/functions.zsh
@@ -0,0 +1,6 @@
+# let jump around using fzf
+unalias z 2> /dev/null
+z() {
+    [ $# -gt 0 ] && _z "$*" && return
+    cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
+}
